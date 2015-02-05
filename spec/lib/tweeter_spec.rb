127354c0ecf2704d438ad941a7b0c43ca3347577
Adam Pash <adam.pash@gmail.com>
2015-02-05 12:07:28 -0500

    Done for now

--- spec/lib/tweeter_spec.rb
+++ spec/lib/tweeter_spec.rb
@@ -2,10 +2,6 @@ require_relative '../../lib/tweeter'
 
 describe Tweeter do
   before :each do
-    @tweeter = Tweeter.new
-  end
-  it "initializes" do
-    expect(@tweeter.client).to be_a Twitter::REST::Client
   end
 
   # it "posts an update" do
@@ -16,20 +12,25 @@ describe Tweeter do
   # end
 
   it "goes nuts" do
+    words = %w(oh wow omg interesting compelling uh-huh seriously! stop! no! fantasticmarvelous wonderful sensational outstanding superb super excellent first-rate! dazzling breathtaking fabulous wicked awesome brilliant extraordinary  unbelievable! remarkable impresssive phenomenal)
     file = File.new "#{File.dirname(__FILE__)}/../../data/tweet_friendly.txt"
     line_num = 1
     file.each_line do |line|
+      puts "Instantiating new rest client"
+      @tweeter = Tweeter.new
+      sleep 3
       puts "tweeting line ##{line_num}"
       puts line
       tweet = @tweeter.update line
       sleep 5
       puts "reply to tweet #{tweet.id}"
-      reply = @tweeter.reply "@MeinCoke #MakeItHappy", tweet
+      reply = @tweeter.reply "@MeinCoke #{words.sample} #MakeItHappy", tweet
       puts "reply id is #{reply.id}"
+      puts "something went wrong" if reply.id == tweet.id
       line_num += 1
       tweet = nil
       reply = nil
-      sleep 31
+      sleep 28
     end
   end
 
